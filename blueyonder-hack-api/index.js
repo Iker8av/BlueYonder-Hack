6999a5177437722d99ccda4c33f36661d3362b5f
Iker8av <iker8avv@gmail.com>
2022-09-24 16:19:56 -0500

    [Back End] - Log In and Sign Up

--- blueyonder-hack-api/index.js
+++ blueyonder-hack-api/index.js
@@ -15,7 +15,55 @@ app.use(cors())
 Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY)
 Parse.serverURL = "https://parseapi.back4app.com"
 
-app.get('/', (req, res) => {
+app.post('/login', async (req, res) => {
+    try {
+      const user = await Parse.User.logIn(req.body.email, req.body.password)
+      res.send({"user" : user})
+    } catch (error) {
+      res.status(400)
+      res.send({"error" : "Login failed: " + error })
+    }
+})
+
+app.post('/register', async (req, res) => {
+    let user = new Parse.User(req.body)
+
+    try {
+        await user.signUp()
+        res.status(201)
+        res.send({"user" : user})
+    } catch (error) {
+        res.status(400)
+        res.send({"error" : "Failed to create user: " + error })
+    }
+})
+
+
+const getLocations = async (data) => {
+    console.log('data: ', data);
+    const query = 'recycling'
+    var config = {
+      method: 'get',
+      url: encodeURI(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${data.lat},${data.lng}&radius=15000&keyword=${query}&key=AIzaSyBZ-L6y4RM_Adga1qdKEj8ZTMCBkMHE_3o`),
+      headers : {
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*"
+      }
+    };
+
+    const results = axios(config)
+    return results
+}
+
+app.post('/getLocations', async (req, res) => {
+    const allLocations = await getLocations(req.body)
+    console.log('allLocations: ', allLocations);
+    res.send("Done").status(200)
+    // res.send(allLocations.data.results).status(200)
+  })
+
+app.get('/helloWorld', (req, res) => {
     res.send('Hello World!')
   })
   
