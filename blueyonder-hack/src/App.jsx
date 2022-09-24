fa2d4bb537912bf0de3148aa9cb3975b0c2c54f5
Iker8av <iker8avv@gmail.com>
2022-09-24 18:16:56 -0500

    Documento separado

--- blueyonder-hack/src/App.jsx
+++ blueyonder-hack/src/App.jsx
@@ -6,13 +6,21 @@ function App() {
   return (
     <div className="App">
       <section className="description" >
-            <div className="text">
-                <h1>Recycle And Live</h1>
-                <p>Recycle And Live is a website that helps you find the nearest recycling center to you. We also provide you with information about recycling and how to recycle.</p>
-            </div>
+        <div className="text">
+            <h1>Recycle And Live</h1>
+            <p>Recycle And Live is a website that helps you find the nearest recycling center to you. We also provide you with information about recycling and how to recycle.</p>
+        </div>
 
-            <img src=""/>   
-        </section>
+        <img src=""/>   
+      </section>
+      {/* Lastest news */}
+      <section>
+
+      </section>
+      {/* Contacto */}
+      <footer>
+
+      </footer>
     </div>
   );
 }
