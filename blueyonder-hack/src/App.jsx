48850b31b9e43f1abab4ef5aac9354ec569975d1
Juan José Salazar Cortés <juansalazar@Juans-MacBook-Air-2.local>
2022-09-25 05:18:40 -0500

    Finished News and Navbar part

--- blueyonder-hack/src/App.jsx
+++ blueyonder-hack/src/App.jsx
@@ -2,21 +2,41 @@ import {GoogleMap, useLoadScript, Marker, DirectionsRenderer} from "@react-googl
 import axios from 'axios'
 import './App.css';
 import Carousel from "./components/Carousel/Carousel";
+import Navbar from "./components/Navbar/Navbar";
+import nature from "../src/img/natureimg.png";
 
 function App() {
   return (
     <div className="App">
-      <section className="description" >
+      <section>
+        <Navbar/>
+      </section>
+      <section className="description">
         <div className="text">
+          <div className="titulo">
             <h1>Recycle And Live</h1>
-            <p>Recycle And Live is a website that helps you find the nearest recycling center to you. We also provide you with information about recycling and how to recycle.</p>
+          </div>
+          <div className="parrafos">
+            <p>
+            “ReciclaTec” offers you a convenient way to connect with recycling centers that manage, 
+            sort, and distribute otherwise wasteful products to the right place. 
+            </p>
+            <p>
+            With comprehensive service capabilities,
+            our service can allow customers to schedule their waste management where it can be taken and used.
+            Stay up to date with news around the world, find useful tips about moving towards a greener path, and much more!
+            Schedule a visit today!
+            </p>
+          </div>
         </div>
-        <img src=""/>   
+        <div className="imagenMain">
+          <img src={nature}/>   
+        </div>
+      </section>
+      <section className="noticias">
+        <h2 className="news">Latest News</h2>
       </section>
         <Carousel/>
-      <section>
-
-      </section>
       {/* Contacto */}
       <footer>
       </footer>
---
+++ blueyonder-hack/src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Logo from "../Navbar/img/logo.png"
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <div className="contenedorMenu">
+                    <div className="Logo">
+                        <img src={Logo} className="logo"></img>
+                    </div>
+                    <div className="todoLoDemas">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Latest News</a>
+                            </li>
+                            
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Select your country
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Mexico</a></li>
+                                <li><a class="dropdown-item" href="#">United States</a></li>
+                                <li><hr class="dropdown-divider"/></li>
+                                <li><a class="dropdown-item" href="#">Rest of the world</a></li>
+                            </ul>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Sign in</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Log in</a>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export  default Navbar;
