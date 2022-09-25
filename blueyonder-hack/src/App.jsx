04def48524947db37393972906bd2d775f4a4af2
Juan José Salazar Cortés <juansalazar@Juans-MacBook-Air-2.local>
2022-09-25 00:02:49 -0500

    Add Carousel News

--- blueyonder-hack/src/App.jsx
+++ blueyonder-hack/src/App.jsx
@@ -1,6 +1,7 @@
 import {GoogleMap, useLoadScript, Marker, DirectionsRenderer} from "@react-google-maps/api"
 import axios from 'axios'
 import './App.css';
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
@@ -10,16 +11,14 @@ function App() {
             <h1>Recycle And Live</h1>
             <p>Recycle And Live is a website that helps you find the nearest recycling center to you. We also provide you with information about recycling and how to recycle.</p>
         </div>
-
         <img src=""/>   
       </section>
-      {/* Lastest news */}
+        <Carousel/>
       <section>
 
       </section>
       {/* Contacto */}
       <footer>
-
       </footer>
     </div>
   );
